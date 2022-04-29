f6e2ac300356af4bec05f77d49050ee1fb500129
Ra1me2sh3 <87349994+Ra1me2sh3@users.noreply.github.com>
2022-04-29 17:32:49 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,24 @@
+public class String1
+  {
+    public static void main(String args[]){
+
+String str1="bitlabs";
+String str2="bitlabs";
+if(str1==str2)
+{
+  System.out.println(true);
+}
+else
+{
+  System.out.println(false);
+}
+if(str1.equals(str2))
+{
+  System.out.println(true);
+}
+else
+{
+  System.out.println(false);
+}
+      }
+  }
